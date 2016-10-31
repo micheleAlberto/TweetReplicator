bfeb1d498bf768701b67a27baba7cc9de1a2b862
michele <robbyetor@gmail.com>
2016-10-31 01:23:36 +0100

    added tweet meta and a data file for obama

---
+++ tweetMeta.py
@@ -0,0 +1,17 @@
+
+
+#info on https://apps.twitter.com/
+
+consumer_key = ""
+consumer_secret = ""
+
+
+access_key = ""
+access_secret = ""
+
+# the user id to be dumped and replicated
+target_user_id= 'barackobama'
+# target_user_id= 'barackobama'
+# for https://twitter.com/barackobama
+
+
